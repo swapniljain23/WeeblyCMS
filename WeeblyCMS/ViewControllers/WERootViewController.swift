74cae5843bbc89e1285891c05994bcf143e5d496
Swapnil Jain <swapnil_jain@apple.com>
2017-09-30 06:52:46 -0700

    Initial Commit

---
+++ WeeblyCMS/ViewControllers/WERootViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeeblyCMS
+//
+//  Created by Swapnil Jain on 9/30/17.
+//  Copyright © 2017 Weebly. All rights reserved.
+//
+
+import UIKit
+
+class WERootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
